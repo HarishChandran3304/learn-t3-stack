e52bd17759a1e8e36e65a9708bc9cbe06a5a27cd
Harish <harish.rajkumar2022@vitstudent.ac.in>
2023-02-07 06:20:41 +0530

    Create basic UI structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className=''>Hello World</div>
+    <div className='h-screen w-screen flex flex-col justify-center align-center items-center'>
+      <div className='text-2xl text-center'>Which JDM car is better?</div>
+      <div className='p-2'></div>
+      <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
+        <div className='w-16 h-16 bg-violet-400'></div>
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-violet-400'> </div>
+      </div>
+    </div>
   )
 }
